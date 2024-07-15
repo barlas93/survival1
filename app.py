117f42132d7dacd2cffd8c5039b7042c55f2dca8
barlas93 <166324074+barlas93@users.noreply.github.com>
2024-07-15 11:32:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
 rf_model.fit(X_resampled, y_resampled)
 
 def main():
-    st.markdown("<h1 style='text-align: center;'>Megaprosthesis Survival Prediction
-    12 months</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>Megaprosthesis Survival Prediction</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>12 months</h1>", unsafe_allow_html=True)
     
     st.sidebar.header('Input Features')
 
